9b79ae88dd1b9f8601745c932a6d200659b592c1
Robin Deleu <deleu.robin@outlook.com>
2021-04-30 16:47:23 +0200

    bigger Default history card

--- frontend/src/components/DefaultHistoryCard.vue
+++ frontend/src/components/DefaultHistoryCard.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container>
-    <v-card class="mx-auto mt-3" max-width="300">
+    <v-card class="mx-auto mt-3" >
       <v-parallax
-        src="https://images.unsplash.com/photo-1618745955167-98654e197a5c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
-        height="200">
+        src="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
+        height="350">
         <v-overlay value="true" absolute>
-            <h3>No devices in history</h3>
+            <h1>No devices in history</h1>
         </v-overlay>
       </v-parallax>
 
